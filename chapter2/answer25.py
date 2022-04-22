5c1cf46cc926b8362048e966e97fe56fee18a739
ryuta-messi <ryutamf8@gmail.com>
2022-04-22 22:53:47 +0900

    done answer25-26

---
+++ chapter2/answer25.py
@@ -0,0 +1,17 @@
+import re
+import pandas as pd
+
+df = pd.read_json('chapter2/jawiki-country.json.gz', lines=True)
+uk_text = df[df['title'] == 'イギリス']['text'].values[0]
+
+uk_texts = uk_text.split('\n')
+
+pattern = re.compile('\|(.+?)\s=\s*(.+)')
+
+ans = {}
+for line in uk_texts:
+    r = re.search(pattern,line)
+    if r:
+        ans[r[1]]=r[2]
+        
+print(ans)
