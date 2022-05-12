4017a8bcec6ee300c03f5b4e75392bd69acc1984
ryuta-messi <ryutamf8@gmail.com>
2022-05-12 22:07:53 +0900

    modify

--- chapter3/answer30.py
+++ chapter3/answer30.py
@@ -4,7 +4,7 @@ import MeCab
 
 filename = "./chapter3/neko.txt.mecab"
 
-with open(filename,mode='rt'k,encoding='utf-8') as f:
+with open(filename,mode='rt',encoding='utf-8') as f:
     file = f.read()
 
 
